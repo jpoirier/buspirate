43fd30da3b430a256ddb2637020ecf35d49e3eca
Joseph Poirier <jdpoirier@gmail.com>
2016-12-03 05:45:20 -0600

    add ignore file, add arduino spi example

---
+++ buspirate-master_arduino-slave_example/arduino_slave/arduino_slave.ino
@@ -0,0 +1,87 @@
+/**
+ * 
+ * 
+ * 
+ */
+
+#include <SPI.h>
+
+byte inBuf[100];
+
+void setup(void) {
+  Serial.begin(115200);
+  Serial.println("Configuring the SPI interface for salve mode...");
+  
+  pinMode(MOSI, INPUT);
+  pinMode(MISO, OUTPUT);
+  pinMode(SCK, INPUT);
+  pinMode(SS, INPUT);
+  digitalWrite(SS, HIGH);
+
+  SPI.beginTransaction(SPISettings(SPI_CLOCK_DIV16, MSBFIRST, SPI_MODE0));
+
+  // turn on SPI in slave mode
+  SPCR |= bit(SPE);
+  
+  // turn on interrupts
+  //SPCR |= bit(SPIE);
+  
+  Serial.println("SPI interface configured...");
+}
+
+//ISR (SPI_STC_vect) {
+//  //Serial.println("isr");
+//  SPDR = 0xFF;
+//  byte rx = SPDR;
+//  Serial.println(rx, DEC);
+//}
+
+byte spi_transfer(byte tx) {
+  SPDR = tx;
+  while (!(SPSR & (1<<SPIF)));
+  return SPDR;
+}
+
+void spi_transfer_block(void) {
+  for (int i = 0; i < 100; i++) {
+    while (!(SPSR & (1<<SPIF)));
+    inBuf[i] = SPDR;
+  }
+
+  for (byte i = 100; i < 200; i++) {
+    SPDR = i;
+    while(!(SPSR & (1<<SPIF)));
+  }
+}
+
+byte rx = 0;
+byte cnt = 20;
+
+bool block_mode = false;
+
+void loop() {
+  if (!digitalRead(SS)) {
+      //Serial.println("LOW");
+      if (!block_mode) {
+        rx = spi_transfer(cnt);
+        Serial.println(rx, DEC);
+        if (rx == 0xFF) {
+          Serial.println("going in to block transfer mode...");
+          block_mode = true;
+        }
+      } else {
+        //Serial.println("calling SPItransferBulk...");
+        spi_transfer_block();
+        for (int i = 0; i < 5; i++) {
+          for (int j = 0; j < 20; j++) {
+            Serial.print(inBuf[i*20+j], DEC);
+            Serial.print(" ");
+          }
+          Serial.println();
+        }
+        Serial.println("finished SPItransferBulk...");
+        block_mode = false;
+      }
+      cnt += 1;
+  }
+}
---
+++ buspirate-master_arduino-slave_example/spi_example.go
@@ -0,0 +1,122 @@
+/*
+Arduino Uno       Bus Pirate
+-----------       ----------
+MOSI pin 11       Gray
+MISO pin 12       Black
+SCK  pin 13       Purple
+SS   pin 10       White
+*/
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/jpoirier/buspirate"
+)
+
+func main() {
+	fmt.Println("opening bp...")
+	bp, err := buspirate.Open("/dev/ttyUSB0")
+	// bp, err := buspirate.Open("/dev/ttyUSB0", 5*time.Second)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	defer bp.LeaveBinaryMode()
+	fmt.Println("serial port is open")
+
+	if err := bp.SpiEnter(); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	defer bp.SpiLeave()
+	fmt.Println("entered spi mode")
+
+	if err := bp.SpiCS(true); err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("CS high")
+
+	if err := bp.SpiSpeed(buspirate.SpiSpeed1mhz); err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("speed set")
+
+	if err := bp.SpiCfg(true, false, false, false); err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("spi cfg done")
+
+	//---
+	fmt.Println("calling SpiSend...")
+	out := []byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16}
+
+	err = bp.SpiCS(false)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	r, err := bp.SpiSend(out)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	err = bp.SpiCS(true)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(r)
+	time.Sleep(2000 * time.Millisecond)
+
+	// ---
+	fmt.Println("sending block mode command...")
+	err = bp.SpiCS(false)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// spi slave cmd for block mode
+	r, err = bp.SpiSend([]byte{0xff})
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	err = bp.SpiCS(true)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(r)
+	time.Sleep(2000 * time.Millisecond)
+
+	// ---
+	fmt.Println("sending sending/reading data block...")
+	out2 := make([]byte, 100)
+	for i := 0; i < 100; i++ {
+		out2[i] = byte(i + 1)
+	}
+	in := make([]byte, 100)
+
+	if err := bp.SpiWriteRead(out2, in); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(in)
+
+	fmt.Println("bye...")
+
+}
